ba2826ef68a4802258de3daf11c2f111d3af5eca
Benoit Pasquier <4486578+briochemc@users.noreply.github.com>
2022-11-25 13:55:40 +1100

    Update offsetaxis.m
    Also keep axis labels and axis tick labels

--- offsetaxis.m
+++ offsetaxis.m
@@ -27,7 +27,7 @@ function varargout = offsetaxis(hax, varargin)
 %           [0.1 if no x-offset is specified, 0 otherwise]
 %
 %   x:      offset for the x-axis, expressed as a fraction of the axis
-%           height.  
+%           height.
 %           [0]
 %
 %   yloc:   string, location for offset y-axis.
@@ -45,10 +45,10 @@ function varargout = offsetaxis(hax, varargin)
 % Output variables:
 %
 %   hy:     handles to offset y-axes, same dimensions as ax input (only
-%           returned if a y-offset is specified by input) 
+%           returned if a y-offset is specified by input)
 %
 %   hx:     handles to offset x-axes, same dimensions as ax input (only
-%           returned if an x-offset is specified by input) 
+%           returned if an x-offset is specified by input)
 
 % Copyright 2017 Kelly Kearney
 
@@ -84,7 +84,7 @@ if ishg2
 else
     ptype = cellfun(@(x) get(x, 'type'), hparent, 'uni', 0);
     pisfig = strcmp(ptype, 'figure');
-    col(pisfig) = cellfun(@(x) get(x, 'color'), hparent(pisfig), 'uni', 0); 
+    col(pisfig) = cellfun(@(x) get(x, 'color'), hparent(pisfig), 'uni', 0);
     col(~pisfig) = cellfun(@(x) get(x, 'backgroundcolor'), hparent(~pisfig), 'uni', 0);
 end
 
@@ -108,11 +108,11 @@ end
 % Create offset axes
 
 for iax = 1:numel(hax)
-    
+
     pos = get(hax(iax), 'position');
-    
+
     if Opt.y > 0
-        
+
         newpos = [pos(1) - pos(3)*Opt.y pos(2) pos(3)+2*Opt.y*pos(3) pos(4)];
         hy(iax) = axes('parent', hparent{iax}, ...
                        'position', newpos, ...
@@ -129,15 +129,17 @@ for iax = 1:numel(hax)
                     set(hy(iax), 'YAxis', matlab.graphics.axis.decorator.CategoricalRuler);
             end
         end
-        
+
         set(hy(iax), ...
             'ylim', get(hax(iax), 'ylim'), ...
             'ydir', get(hax(iax), 'ydir'), ...
-            'TickDir', get(hax(iax), 'TickDir') ...
+            'TickDir', get(hax(iax), 'TickDir'), ...
+            'yticklabel', get(hax(iax), 'yticklabel'), ...
+            'ylabel', get(hax(iax), 'ylabel') ...
         );
         hlink = linkprop([hax(iax) hy(iax)], 'YLim');
         setappdata(hax(iax), 'yoffsetlink', hlink);
-        
+
         switch Opt.yloc
             case {'lr', 'rl'}
                 set(hy(iax), 'box', 'on');
@@ -148,7 +150,7 @@ for iax = 1:numel(hax)
         end
         uistack(hy(iax), 'bottom');
     end
-    
+
     if Opt.x > 0
         newpos = [pos(1) pos(2)-pos(4)*Opt.x pos(3) pos(4)+2*pos(4)*Opt.x];
         hx(iax) = axes('parent', hparent{iax}, ...
@@ -167,15 +169,16 @@ for iax = 1:numel(hax)
         end
         set(hax(iax), 'xcolor', col{iax}, 'xtick', []);
 
-
         set(hx(iax), ...
             'xlim', get(hax(iax), 'xlim'), ...
             'xdir', get(hax(iax), 'xdir'), ...
-            'TickDir', get(hax(iax), 'TickDir') ...
+            'TickDir', get(hax(iax), 'TickDir'), ...
+            'xticklabel', get(hax(iax), 'xticklabel'), ...
+            'xlabel', get(hax(iax), 'xlabel') ...
         );
         hlink = linkprop([hax(iax) hx(iax)], 'XLim');
         setappdata(hax(iax), 'xoffsetlink', hlink);
-        
+
         switch Opt.xloc
             case {'tb', 'bt'}
                 set(hx(iax), 'box', 'on');
@@ -186,7 +189,7 @@ for iax = 1:numel(hax)
         end
         uistack(hx(iax), 'bottom');
     end
-    
+
 end
 
 % Return handles
@@ -198,5 +201,5 @@ elseif addx
 elseif addy
     varargout = {hy};
 end
-    
-    
+
+
